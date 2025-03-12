7c8e6e1c291db2b04597e051d972c216f593d835
sowmika03 <vemulasaisowmika@gmail.com>
2025-03-12 18:32:51 +0530

    Create 3 and or xor.py

---
+++ 3 and or xor.py
@@ -0,0 +1,7 @@
+astr=input ()
+if(astr[1]=='a'):
+    print(int(astr[0]) and int(astr[2]))
+elif(astr[1]=='b'):
+    print(int(astr[0]) or int(astr[2]))
+elif(astr[1]=='c'):
+    print(int(astr[0]) ^ int(astr[2]))
