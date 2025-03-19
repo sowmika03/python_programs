435383d84e5018430210cb3d699ea1fbdb099944
sowmika03 <vemulasaisowmika@gmail.com>
2025-03-20 00:19:38 +0530

    Create 22.Internal Varsity Square Pattern.py

---
+++ 22.Internal Varsity Square Pattern.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+    for j in range(1,4):
+        if i==2 and j==2:
+            print("1",end="")
+        elif i==3 and j==2:
+            print("2",end="")
+        else:
+            print("3",end="")
+    print()
