cdefb052490f9d6cf1ac1f29241a7ee60cbc032c
sowmika03 <vemulasaisowmika@gmail.com>
2025-03-20 00:17:45 +0530

    Create 21.Basic Square Incrementing Pattern.py

---
+++ 21.Basic Square Incrementing Pattern.py
@@ -0,0 +1,11 @@
+for i in range(1,5):
+    for j in range(1,5):
+        if i==1:
+            print("1",end="")
+        elif i==2:
+            print("2",end="")
+        elif i==3:
+            print("3",end="")
+        else:
+            print("4",end="")
+    print()
